deb1e78a9a03929868fa4f5db22671e299898500
sigmaangelo <angelo.frank.george@gmail.com>
2025-11-29 10:11:54 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,22 @@
 import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
 
-Deno.serve((req) =>
-  serveDir(req, {
+Deno.serve((req) => {
+  const url = new URL(req.url);
+
+  // Fix Unity WASM MIME type
+  if (url.pathname.endsWith(".wasm")) {
+    return fetch(req.url).then((res) =>
+      new Response(res.body, {
+        headers: {
+          "content-type": "application/wasm",
+        },
+      })
+    );
+  }
+
+  return serveDir(req, {
     fsRoot: ".",
-    showDirListing: true, // Optional: shows folder list if no index.html
-    enableCors: true,     // Helps when you embed with iframe
-  })
-);
+    showDirListing: true,
+    enableCors: true,
+  });
+});
