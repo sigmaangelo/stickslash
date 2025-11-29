8fd73acb5c0a5b3cb369f9c7cc0d0c8ecf56871a
sigmaangelo <angelo.frank.george@gmail.com>
2025-11-29 10:21:34 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,13 @@
 import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
 
-Deno.serve((req) => {
+Deno.serve(async (req) => {
   const url = new URL(req.url);
 
-  // Fix Unity WASM MIME type
   if (url.pathname.endsWith(".wasm")) {
-    return fetch(req.url).then((res) =>
-      new Response(res.body, {
-        headers: {
-          "content-type": "application/wasm",
-        },
-      })
-    );
+    const file = await Deno.readFile("." + url.pathname);
+    return new Response(file, {
+      headers: { "Content-Type": "application/wasm" },
+    });
   }
 
   return serveDir(req, {
